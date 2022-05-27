44e38b3439dfa932b97fa75529e2d5d4a8ec9120
Kenny Gorman <kenny.gorman@mongodb.com>
2022-05-27 11:54:13 -0500

    updated to use new name via GH integrations

--- realm/functions/Atlas_Triggers_lightningProd_1651502902.js
+++ realm/functions/Atlas_Triggers_lightningProd_1651502902.js
@@ -2,33 +2,39 @@ exports = function(changeEvent) {
 
     const fullDocument = changeEvent.fullDocument;
     const collection = context.services.get("KGShardedCluster01").db("demo").collection("strikes");
+    const usersCollection = context.services.get("KGShardedCluster01").db("demo").collection("users"); 
     const logCollection = context.services.get("KGShardedCluster01").db("demo").collection("strikeslog");
     strikeFound = 0;
+    messageAck = "";
 
-    if (fullDocument && fullDocument.obs[0].lightning_strike_count_last_1hr < 100 && fullDocument.obs[0].lightning_strike_last_distance < 50 ) {
+    /* test for a strike near me */
+    if (fullDocument && fullDocument.obs[0].lightning_strike_count_last_1hr > 2 && fullDocument.obs[0].lightning_strike_last_distance < 20 ) {
         const strikeDocument = {
             'lightning_strike_last_epoch' : fullDocument.obs[0].lightning_strike_last_epoch,
             'lightning_strike_last_distance' : fullDocument.obs[0].lightning_strike_last_distance,
             'lightning_strike_count' : fullDocument.obs[0].lightning_strike_count,
             'lightning_strike_count_last_1hr' : fullDocument.obs[0].lightning_strike_count_last_1hr,
-            'lightning_strike_count_last_3hr' : fullDocument.obs[0].lightning_strike_count_last_3hr
+            'lightning_strike_count_last_3hr' : fullDocument.obs[0].lightning_strike_count_last_3hr,
         };
 
-        /* send an alert, be sure to add twilio to dependencies */
-        const accountSid = 'AC4d0ea8d08742f70ba5aff90da6bbe10a'; 
-        const authToken = '3446d37f748a0c91d833a600cd95d272'; 
-        const client = require('twilio')(accountSid, authToken, { lazyLoading: true }); 
- 
-        /* comment out for now
-        client.messages 
-            .create({ 
-                body: 'lightning strike detected near you!',
-                messagingServiceSid: 'MGb82eed03aea26d50742a67bfb59a851c',      
-                to: '+19255773168' 
-            }) 
-            .then(message => console.log('message sent')) 
-            .done();
-        */
+        /* look for userid to alert, if not skip */
+        const user = usersCollection.findOne({"station_id": fullDocument.station_id })
+        if (user) {
+
+            /* send an alert, be sure to add twilio to dependencies */
+            const accountSid = "%%values.twilio_account_sid"; 
+            const authToken = "%%values.twilio_auth_token";
+            const client = require('twilio')(accountSid, authToken, { lazyLoading: true });
+    
+            messageAck = client.messages 
+                .create({ 
+                    body: 'lightning strike detected near you!',
+                    messagingServiceSid: "%%values.twilio_service_sid",
+                    to: user.sms
+                }) 
+                .then(message => console.log('message sent')) 
+                .done();
+            }
 
         /* upsert a collection */
         const pk = fullDocument.station_id;
@@ -44,7 +50,12 @@ exports = function(changeEvent) {
 
     /* log what this run found */
     if (fullDocument) {
-        const logDocument = { station_id: fullDocument.station_id, strikeFound: strikeFound, createdAt: fullDocument.createdAt };
+        const logDocument = { 
+                        station_id: fullDocument.station_id, 
+                        strikeFound: strikeFound,
+                        messageLog: messageAck,
+                        createdAt: fullDocument.createdAt
+                        };
         logCollection.insertOne(logDocument);
     }
 
