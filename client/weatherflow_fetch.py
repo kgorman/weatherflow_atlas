5681a9a37ffe6a9d29d6fe9e5e0a6aa54d9dea6e
Kenny Gorman <kenny.gorman@mongodb.com>
2024-02-21 11:42:27 -0600

    mods for using mongodb URI/driver

--- client/weatherflow_fetch.py
+++ client/weatherflow_fetch.py
@@ -2,16 +2,17 @@ import requests
 import json
 import os
 import time
-
-import socket
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
 SOURCE_BASE_URL = os.environ.get('SOURCE_BASE_URL')
 SOURCE_KEY = os.environ.get('SOURCE_KEY')
 SOURCE_STATION = os.environ.get('SOURCE_STATION')
 
-TARGET_BASE_URL = os.environ.get('TARGET_BASE_URL')
-TARGET_KEY = os.environ.get('TARGET_KEY')
-TARGET_NAME = os.environ.get('TARGET_NAME')
+USERNAME = os.environ.get('TARGET_USERNAME')
+PASSWORD = os.environ.get('TARGET_PASSWORD') 
+TARGET_URL = os.environ.get('TARGET_URL')
+MONGOURI = "mongodb+srv://{}:{}@{}".format(USERNAME, PASSWORD, TARGET_URL)
 TARGET_COLLECTION = os.environ.get('TARGET_COLLECTION')
 TARGET_DATABASE = os.environ.get('TARGET_DATABASE')
 
@@ -25,29 +26,19 @@ def fetch():
     print(d)
     return d
 
-def send_to_atlas(payload):
-    headers = {
-        'Content-Type': 'application/json',
-        'Access-Control-Request-Headers': '*',
-        'api-key': TARGET_KEY
-    }
-    raw_data = json.dumps({
-            "dataSource": TARGET_NAME,
-            "database": TARGET_DATABASE,
-            "collection": TARGET_COLLECTION,
-            "document": payload
-    })
-    response = requests.post(TARGET_BASE_URL, headers=headers, data=raw_data)
-    if response.status_code != 201:
-        raise ValueError("Error code was {}".format(response.status_code))
-    print("flushed to atlas")
+def sendDataAtlas(d):
+   mongoclient = MongoClient(MONGOURI)
+   db = mongoclient[TARGET_DATABASE]
+   col = db[TARGET_COLLECTION]
+   print(json.dumps(d))
+   col.insert_one(d)
 
 def main():
     while True:
         try:
             d = fetch()
-            if TARGET_BASE_URL:
-                send_to_atlas(d)
+            if MONGOURI:
+                sendDataAtlas(d)
         except Exception as e:
             print("ERROR: unable to process data {}".format(e))
         time.sleep(30)
